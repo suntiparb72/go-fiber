e6f5d29ff7335bed5179736cd0be28ec034ef7e2
Suntiparb <pae@Suntiparbs-MacBook-Air.local>
2024-04-08 15:22:17 +0700

    feat(updateBook): update book with id

--- main.go
+++ main.go
@@ -27,7 +27,8 @@ func main() {
 
 	app.Get("/books", getBooks)
 	app.Get("/books/:id", getBook)
-	app.Post("/books", createBooks)
+	app.Post("/books", createBook)
+	app.Put("/books/:id", updateBook)
 
 	app.Listen("localhost:8080")
 }
@@ -55,7 +56,8 @@ func getBook(c *fiber.Ctx) error {
 	}
 	return c.Status(fiber.StatusNotFound).SendString("Not found eiei")
 }
-func createBooks(c *fiber.Ctx) error {
+
+func createBook(c *fiber.Ctx) error {
 	// c.Params()
 	book := new(Book)
 
@@ -66,4 +68,30 @@ func createBooks(c *fiber.Ctx) error {
 	books = append(books, *book)
 
 	return c.JSON(book)
+}
+
+func updateBook(c *fiber.Ctx) error {
+
+	bookId, err := strconv.Atoi(c.Params("id"))
+
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+	}
+
+	bookUpdate := new(Book)
+
+	if err := c.BodyParser(bookUpdate); err != nil {
+		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+	}
+
+	for i, book := range books {
+		if book.ID == bookId {
+			books[i].Title = bookUpdate.Title
+			books[i].Author = bookUpdate.Author
+			c.JSON(books[i])
+		}
+	}
+
+	return c.SendStatus(fiber.StatusNotFound)
+
 }
