9272f4d7258e62f308103d7f8a852ed9137c8b95
Suntiparb <pae@Suntiparbs-MacBook-Air.local>
2024-04-08 10:46:53 +0700

    feat(getbooks): getbooks API

--- main.go
+++ main.go
@@ -4,6 +4,14 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+type Book struct {
+	ID     int    `json:"id"`
+	Title  string `json:"title"`
+	Author string `json:"author"`
+}
+
+var books []Book
+
 func main() {
 	app := fiber.New()
 
@@ -11,5 +19,23 @@ func main() {
 		return c.SendString("Hello World!")
 	})
 
-	app.Listen(":8080")
+	books = append(books, Book{ID: 1, Title: "Suntiparb", Author: "Pae"})
+	books = append(books, Book{ID: 2, Title: "MM", Author: "Pae"})
+
+	// print(books)
+
+	app.Get("/books", getBooks)
+	app.Get("/books/:id", getBook)
+
+	app.Listen("localhost:8080")
+}
+
+func getBooks(c *fiber.Ctx) error {
+	return c.JSON(books)
+}
+
+func getBook(c *fiber.Ctx) error {
+	bookId := c.Params("id")
+	return c.SendString(bookId)
+	// return c.JSON(books[0])
 }
