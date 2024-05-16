ddfd8156cfb9110238c1b704a1d9045c3e616f83
Suntiparb <pae@Suntiparbs-MacBook-Air.local>
2024-05-16 15:06:51 +0700

    refactor(main-book): move book func -> book.go

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"strconv"
-
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -31,79 +29,4 @@ func main() {
 	app.Delete("/books/:id", deleteBook)
 
 	app.Listen("localhost:8080")
-}
-
-func getBooks(c *fiber.Ctx) error {
-	return c.JSON(books)
-}
-
-func getBook(c *fiber.Ctx) error {
-	// convert string => int
-	bookId, err := strconv.Atoi(c.Params("id"))
-
-	if err != nil {
-		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
-	}
-
-	for _, book := range books {
-		if book.ID == bookId {
-			return c.JSON(book)
-		}
-	}
-	return c.Status(fiber.StatusNotFound).SendString("Not found eiei")
-}
-
-func createBook(c *fiber.Ctx) error {
-
-	book := new(Book)
-
-	// check err before use BodyParser
-	if err := c.BodyParser(book); err != nil {
-		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
-	}
-	books = append(books, *book)
-
-	return c.JSON(book)
-}
-
-func updateBook(c *fiber.Ctx) error {
-
-	bookId, err := strconv.Atoi(c.Params("id"))
-
-	if err != nil {
-		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
-	}
-
-	bookUpdate := new(Book)
-	if err := c.BodyParser(bookUpdate); err != nil {
-		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
-	}
-
-	for i, book := range books {
-		if book.ID == bookId {
-			books[i].Title = bookUpdate.Title
-			books[i].Author = bookUpdate.Author
-			c.JSON(books[i])
-		}
-	}
-	return c.SendStatus(fiber.StatusNotFound)
-}
-
-func deleteBook(c *fiber.Ctx) error {
-
-	bookId, err := strconv.Atoi(c.Params("id"))
-
-	if err != nil {
-		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
-	}
-
-	for i, book := range books {
-		if book.ID == bookId {
-			books = append(books[:i], books[i+1:]...)
-			return c.SendStatus(fiber.StatusNoContent)
-		}
-	}
-
-	return c.SendStatus(fiber.StatusNotFound)
-
 }
